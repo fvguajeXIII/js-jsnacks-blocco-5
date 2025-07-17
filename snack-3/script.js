f95bb025f1d2f30878365d6cfa43bd02cda4156f
fvguajexiii <f.vellucci13@outlook.it>
2025-07-17 13:59:22 +0200

    Esercizio 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,7 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const numeriIncrementati = numbers.map(numbers => numbers + 1);
+console.log(numeriIncrementati)
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]

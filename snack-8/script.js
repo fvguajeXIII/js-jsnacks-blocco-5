1661634805c6eab63af1a13f66648a6acc961039
fvguajexiii <f.vellucci13@outlook.it>
2025-07-17 14:15:56 +0200

    Esercizio 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -19,5 +19,7 @@ const students = [
   },
 ];
 
+const studente = students.find(student => student.name === 'Marco Lanci');
+console.log(studente.class);
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
